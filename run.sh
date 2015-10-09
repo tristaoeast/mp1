30a1fdde84c4690e793e853f83275e496b9af2d3
tmic <tristaoeast@gmail.com>
2015-10-09 18:15:09 +0100

    Tweaking the script

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-rm -f *.pdf *.fst
-rm -f 1-inputs_condensados/*.fst 1-inputs_condensados/*.pdf
-rm -f 1-outputs_lf/*.fst 1-outputs_lf/*.pdf
-rm -f 2-inputs_lf/*.fst 2-inputs_lf/*.pdf
-rm -f 2-outputs_cond/*.fst 2-outputs_cond/*.pdf
+# rm -f *.pdf *.fst
+# rm -f 1-inputs_condensados/*.fst 1-inputs_condensados/*.pdf
+# rm -f 1-outputs_lf/*.fst 1-outputs_lf/*.pdf
+# rm -f 2-inputs_lf/*.fst 2-inputs_lf/*.pdf
+# rm -f 2-outputs_cond/*.fst 2-outputs_cond/*.pdf
 
+./clean.sh
 
 ################### letras ################
 #
